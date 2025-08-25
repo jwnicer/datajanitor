37e1e84cf0aafa5364b4ed6b5c89c5e6a7794c63
jwnicer <jhnicer@alum.up.edu.ph>
2025-08-25 16:26:15 +0000

    WIP: local changes

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -3,26 +3,30 @@
 {pkgs}: {
   # Which nixpkgs channel to use.
   channel = "stable-24.11"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+
+  # Packages available in the environment
   packages = [
     pkgs.nodejs_20
     pkgs.zulu
+    pkgs.undollar       # adds $ command permanently
+    pkgs.firebase-tools # Firebase CLI via Nix
   ];
+
   # Sets environment variables in the workspace
   env = {};
-  # This adds a file watcher to startup the firebase emulators. The emulators will only start if
-  # a firebase.json file is written into the user's directory
+
+  # Firebase emulator settings
   services.firebase.emulators = {
-    # Disabling because we are using prod backends right now
     detect = false;
     projectId = "demo-app";
     services = ["auth" "firestore"];
   };
+
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
     ];
+
     workspace = {
       onCreate = {
         default.openFiles = [
@@ -30,7 +34,7 @@
         ];
       };
     };
-    # Enable previews and customize configuration
+
     previews = {
       enable = true;
       previews = {
